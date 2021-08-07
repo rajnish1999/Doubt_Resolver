5dba50937a893ed4c4439652285a20f70f4369c0
rajnish1999 <rajnishtiwarihrt@gmail.com>
2021-08-07 20:09:37 +0530

    Added reply on comments featur

--- routes/comment.js
+++ routes/comment.js
@@ -5,16 +5,24 @@ const Comment = require('../database/models/comment')
 const isAuth = require('./authMiddleware');
 
 router.post('/addComment', isAuth, async (req, res) => {
-    const { comment:newComment, question, answer } = req.body;
 
-    const comment = new Comment({
-        "commentText": newComment,
-        "creator": req.user._id,
-        "question": question,
-        "answer": answer,
-    })
-
-    await comment.save();
+    if(req.body.parent){
+        const { comment:newComment, parent } = req.body;
+        const comment = new Comment({
+            "parentComment": parent,
+            "commentText": newComment
+        })
+        await comment.save();
+    }else {
+        const { comment:newComment, question, answer } = req.body;
+        const comment = new Comment({
+            "commentText": newComment,
+            "creator": req.user._id,
+            "question": question,
+            "answer": answer,
+        })
+        await comment.save();
+    }
 
     res.redirect('/');
 })
